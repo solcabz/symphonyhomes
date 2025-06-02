d83d274ed7e116b3c1ba73d8ba394fa22b9d7f8b
solcabz <solcabreza@gmail.com>
2025-06-02 10:31:39 +0800

    git location carousel

---
+++ templates/home-module/location-block.php
@@ -0,0 +1,25 @@
+<section class="location-section">
+  <div class="location-wrapper">
+    <h1>Our Locations</h1>
+    <p>Strategically situated near essential establishments, making life more convenient for residents. You’ll find schools, hospitals, government offices, grocery stores, and places of worship all within easy reach. Additionally, our locations are in proximity to popular tourist destinations, allowing you to enjoy leisure activities and cultural experiences right at your doorstep.</p>
+
+    <div class="carousel-container">
+      <button class="carousel-btn prev">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M165.66,202.34a8,8,0,0,1-11.32,11.32l-80-80a8,8,0,0,1,0-11.32l80-80a8,8,0,0,1,11.32,11.32L91.31,128Z"></path></svg>
+      </button>
+      
+      <div class="carousel-track">
+        <div class="location-item">1</div>
+        <div class="location-item">2</div>
+        <div class="location-item">3</div>
+        <div class="location-item">4</div>
+        <div class="location-item">5</div>
+        <div class="location-item">6</div>
+      </div>
+
+      <button class="carousel-btn next">
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
+      </button>
+    </div>
+  </div>
+</section>
--- templates/home-template.php
+++ templates/home-template.php
@@ -7,3 +7,4 @@
 
 <?php include get_template_directory() . '/templates/home-module/home-hero.php'; ?>
 <?php include get_template_directory() . '/templates/home-module/about-block.php'; ?>
+<?php include get_template_directory() . '/templates/home-module/location-block.php'; ?>
